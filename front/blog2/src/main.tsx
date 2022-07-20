6b2fb9b69cdd623e4e52bb65dd0c927374ee3bee
yuidai <hoppepoipoi@gmail.com>
2022-07-21 01:31:08 +0900

    add msw urql

--- front/blog2/src/main.tsx
+++ front/blog2/src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createClient, Provider} from "urql";
 import App from './App';
 import './index.css';
 
+const client = createClient({
+  url: 'http://localhost:3000/graphql',
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider value={client}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
